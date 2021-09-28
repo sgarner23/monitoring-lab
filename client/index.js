3e77f6a6e0a7213b730949b07a2e4890e6a1b633
Stephen Garner <stephenallen.garner@gmail.com>
2021-09-28 15:22:05 -0600

    displaying text

--- client/index.js
+++ client/index.js
@@ -2,9 +2,11 @@ gsap.from(".btn", {rotation: 90,
   x: 800, duration: 1.2, opacity: 0});
 
   const btn = document.querySelector('.btn')
+  const responseBox = document.querySelector('.response')
 
   btn.addEventListener('click', () =>{
     axios.get('/api/nice-things').then((res) => {
       console.log(res.data)
+      responseBox.textContent = res.data;
     })
   })
